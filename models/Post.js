ea22562fc09fe92741d1f447cd19cf77b571101e
Greg Tyler <greg@gregtyler.co.uk>
2016-10-08 17:52:06 +0100

    Add comments

--- models/Post.js
+++ models/Post.js
@@ -5,15 +5,19 @@ module.exports.find = function find(id) {
   let matchFound = false;
 
   return new Promise(function(resolve, reject) {
+    // Look through the data directory
     fs.readdir('data', function(err, files) {
       if (err) reject(err);
       for (const filename of files) {
+        // Check if the filename matches what we're searching for
         if (filename.substr(9, filename.length - 12) === id) {
           matchFound = true;
+          // Get the contents of the file
           fs.readFile(`data/${filename}`, 'utf-8', function(err, contents) {
             if (err) {
               reject(err);
             } else {
+              // Get the post details out of the file
               const post = parsePostFile(contents);
               resolve(post);
             }
@@ -21,6 +25,7 @@ module.exports.find = function find(id) {
         }
       }
 
+      // Throw an error if no post was found
       if (!matchFound) {
         reject(new Error('Post not found'));
       }
