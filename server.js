65517b43cd2e72b3426342d414f917bbb10d55c4
Greg Tyler <greg@gregtyler.co.uk>
2016-10-10 21:56:38 +0100

    Sort out 404 errors more consistently

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Load the HTTP module
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 const Templater = require('./lib/Templater');
 const Post = require('./models/Post');
 
@@ -8,16 +9,50 @@ const Post = require('./models/Post');
 const PORT = 4141;
 
 /**
- * Handle an error
+ * Handle an error. Example usage: .catch(handleError.bind(process, response))
  * @param Error The error that was thrown
  */
-/*function handleError(response, err) {
-  response.statusCode = 404;
+/*function responseErrorInternal(response, err) {
+  response.statusCode = 503;
+  response.setHeader('Content-type', 'text/html');
   const errorId = 'E-' + Date.now();
   fs.appendFile('error.log', `${errorId}: ${err.message}\n`);
-  response.end(Templater.render('error.nunjucks', {errorId}));
+  response.end(Templater.render('errors/internal.nunjucks', {errorId}));
 }*/
-// .catch(handleError.bind(process, response))
+
+/**
+ * Handle an error. Example usage: .catch(handleError.bind(process, response))
+ * @param Error The error that was thrown
+ */
+function responseError404(response) {
+  response.statusCode = 404;
+  response.setHeader('Content-type', 'text/html');
+  response.end(Templater.render('errors/404.nunjucks'));
+}
+
+const contentType = {
+  '.css': 'text/css',
+  '.jpeg': 'image/jpeg',
+  '.jpg': 'image/jpg',
+  '.png': 'image/png'
+};
+
+/**
+ * Try to pass through a file, but throw a 404 if it couldn't be found
+ */
+function passFile(response, filepath) {
+  fs.readFile(filepath, 'utf8', function(err, contents) {
+    if (err) {
+      return responseError404(response);
+    }
+
+    const extension = path.extname(filepath);
+
+    response.statusCode = 200;
+    response.setHeader('Content-type', contentType[extension.toLowerCase()]);
+    response.end(contents);
+  });
+}
 
 /**
  * Handle a request to the server
@@ -26,36 +61,21 @@ const PORT = 4141;
  */
 function handleRequest(request, response) {
   if (request.url === '/main.css') {
-    response.statusCode = 200;
-    response.setHeader('Content-type', 'text/css');
-    fs.readFile('public' + request.url, function(err, contents) {
-      if (err) throw err;
-      response.end(contents);
-    });
-  } else if (request.url.match(/\/assets\/user-images\/([a-z0-9-_ ]+\.([a-z0-9]+))/)) {
-    const a = request.url.match(/\/assets\/user-images\/([a-z0-9-_ ]+\.[a-z0-9]+)/);
-    response.statusCode = 200;
-    response.setHeader('Content-type', 'image/jpeg');
-    fs.readFile('data/images/' + a[1], function(err, contents) {
-      if (err) throw err;
-      response.end(contents);
-    });
+    passFile(response, 'public/main.css');
+  } else if (request.url.match(/^\/assets\/user-images\/([a-z0-9-_ ]+\.([a-z0-9]+))$/)) {
+    const match = request.url.match(/^\/assets\/user-images\/([a-z0-9-_ ]+\.[a-z0-9]+)$/);
+    passFile(response, 'data/images/' + match[1]);
   } else if (request.url === '/') {
     response.end(Templater.render('home.nunjucks'));
-  } else {
+  } else if (request.url.match(/^\/([a-z0-9-_])$/i)) {
     // Find the requested post
     const postId = request.url.substr(1);
     Post.find(postId).then(function(post) {
       // Show the post
       response.end(Templater.render('post.nunjucks', post));
-    }).catch(function() {
-      // If the post wasn't found, show an error message
-      response.statusCode = '404';
-      response.end(Templater.render('error.nunjucks', {
-        title: 'Post not found',
-        message: 'The requested post could not be found. It may have been moved, or deleted or renamed.'
-      }));
-    });
+    }).catch(responseError404.bind(process, response));
+  } else {
+    responseError404(response);
   }
 }
 
