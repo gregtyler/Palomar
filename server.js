84a98b4eb164320a752014414d8c0b7a7cafddd8
Greg Tyler <greg@gregtyler.co.uk>
2016-10-11 18:17:28 +0100

    Create new router for each request to allow parallel processing

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // Load the HTTP module
 const http = require('http');
 const Router = require('./lib/Router');
-const router = new Router();
 
 // Define server port (proxied to URL through Apache)
 const PORT = 4141;
 
 // Create a server and register the resposne function
-const server = http.createServer(router.handleRequest.bind(router));
+const server = http.createServer(function() {
+  const router = new Router();
+  router.handleRequest.apply(router, arguments);
+});
 
 // Start the server
 server.listen(PORT, function() {
