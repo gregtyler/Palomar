9cac3e8289c48fa84a639a430340aa9e6739fdff
Greg Tyler <greg@gregtyler.co.uk>
2016-11-05 21:02:02 +0000

    Add new favicon

--- models/Post.js
+++ models/Post.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const parsePostFile = require('../lib/parsePostFile');
 
-module.exports.find = function find(id) {
+module.exports.all = function find(criteria) {
   let matchFound = false;
 
   return new Promise(function(resolve, reject) {
     // Look through the data directory
-    fs.readdir('data', function(err, files) {
+    fs.readdir(`data/${series}`, function(err, files) {
       if (err) reject(err);
       for (const filename of files) {
         // Check if the filename matches what we're searching for
         if (filename.substr(9, filename.length - 12) === id) {
           matchFound = true;
           // Get the contents of the file
-          fs.readFile(`data/${filename}`, 'utf-8', function(err, contents) {
+          fs.readFile(`data/${series}/${filename}`, 'utf-8', function(err, contents) {
             if (err) {
               reject(err);
             } else {
@@ -31,4 +31,9 @@ module.exports.find = function find(id) {
       }
     });
   });
+};
+
+module.exports.find = function(criteria) {
+  const all = this.all(criteria);
+  return (all.length === '' ? null : all[0]);
 };
