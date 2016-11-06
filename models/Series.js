e3f4343101000358b86dd85dd6ce8900c14c2b90
Greg Tyler <greg@gregtyler.co.uk>
2016-11-06 23:16:23 +0000

    Fix problem with sorting series before all had been found

--- models/Series.js
+++ models/Series.js
@@ -4,32 +4,34 @@ const path = require('path');
 const indexFilename = '_index.json';
 
 module.exports.all = function all(criteria) {
-  const all = [];
-
   if (typeof criteria !== 'object') criteria = {};
 
   return new Promise(function(resolve, reject) {
+    const proms = [];
     fs.readdir('data', function(err, files) {
       if (err) reject(err);
       for (let index = 0, l = files.length; index < l; index++) {
         const file = files[index];
         const filepath = path.resolve('data', file);
-        fs.stat(filepath, function(err, stat) {
-          if (err) reject(err);
-          if (stat && stat.isDirectory()) {
-            fs.readFile(`${filepath}/${indexFilename}`, function(err, contents) {
-              if (err) return; // File doesn't exist
-              const details = JSON.parse(contents);
-              all.push({label: details.label, url: `/${file}`});
-            });
-
-            // If that's the end of the loop then quit
-            if (index === files.length - 1) {
-              resolve(all);
+        proms.push(new Promise(function(resolve, reject) {
+          fs.stat(filepath, function(err, stat) {
+            if (err) reject(err);
+            if (stat && stat.isDirectory()) {
+              fs.readFile(`${filepath}/${indexFilename}`, function(err, contents) {
+                if (err) return resolve(null); // File doesn't exist
+                const details = JSON.parse(contents);
+                resolve({label: details.label, url: `/${file}`});
+              });
+            } else {
+              resolve(null);
             }
-          }
-        });
+          });
+        }));
       }
+
+      Promise.all(proms).then(function(series) {
+        resolve(series.filter(a => !!a));
+      });
     });
   }).then(function(series) {
     // Sort series if requested
@@ -43,7 +45,7 @@ module.exports.all = function all(criteria) {
       return (orderRev ? -1 : 1) * (a[orderParam] > b[orderParam] ? 1 : -1);
     });
 
-    // Return posts
+    // Return series
     return series;
   });
 };
