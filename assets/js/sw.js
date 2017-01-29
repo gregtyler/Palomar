f745329cf8d3c011c02ae6c95fad027086878df7
Greg Tyler <greg@gregtyler.co.uk>
2017-01-29 13:16:37 +0000

    Service worker improvements inc. offline page

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -2,7 +2,8 @@
 /* eslint-disable */
 const CACHE_NAME = 'gregtyler-cache';
 const urlsToCache = [
-  '/'
+  '/',
+  '/offline'
 ];
 
 self.addEventListener('install', function(event) {
@@ -33,6 +34,11 @@ self.addEventListener('fetch', function(event) {
 
         fetchRequest.mode = 'no-cors';
 
+        // Don't try and handle about:blank pages
+        if (fetchRequest.url === 'about:blank') {
+          return new Response('',  {headers: {'Content-Type': 'text/html'}});
+        }
+
         return fetch(fetchRequest).then(
           function(response) {
             // Check if we received a valid response
@@ -53,7 +59,17 @@ self.addEventListener('fetch', function(event) {
 
             return response;
           }
-        );
+        ).catch(function() {
+          // Offline
+          if (fetchRequest.headers.get('Accept').indexOf('text/html') !== -1) {
+            // If the request is for a page, show an offline message
+            return caches.match('/offline/');
+          } else if (fetchRequest.headers.get('Accept').indexOf('image') !== -1) {
+            return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
+          } else {
+            // CSS/JS?
+          }
+        });
       })
     );
 });
