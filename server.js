e1049a7803753ee0135cd88f3d6cc70208c7ff86
Greg Tyler <greg@gregtyler.co.uk>
2017-08-04 11:36:19 +0100

    Rewrite Router for expansion
    Splits Router into main routing component, response definitions and routes
    
    Fixes #35

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Load the HTTP module
 const http = require('http');
 const Router = require('./lib/Router');
+const respond = require('./lib/respond');
+const routes = require('./lib/routes');
 
 // Define server port (proxied to URL through Apache)
 const PORT = 4141;
@@ -8,6 +10,19 @@ const PORT = 4141;
 // Create a server and register the resposne function
 const server = http.createServer(function() {
   const router = new Router();
+
+  // Define error responses
+  router.notFound(() => {
+    return respond.template('errors/404.nunjucks', {}, {'Content-type': 'text/html', status: 404});
+  });
+
+  router.error(() => {
+    return respond.text('500 error occurred', {'Content-type': 'text/plain', status: 500});
+  });
+
+  // Load Palomar routes
+  routes(router);
+
   router.handleRequest.apply(router, arguments);
 });
 
